a2c53bb21bf62769fa7491722e217d1a40f7a99e
Casey Morris <casey.morris87@gmail.com>
2021-12-07 11:59:37 -0800

    numbers

---
+++ py_numbers/numbers_ex1.py
@@ -0,0 +1,15 @@
+num1 = input('Enter num: ')
+num2 = input('Enter num: ')
+
+num1 = int(num1)
+num2 = int(num2)
+
+num_sum = num1 + num2
+num_diff = num1 - num2
+num_prod = num1 * num2
+num_quo = num1 / num2 
+
+print(f'The Sum of {num1} and {num2} is: {num_sum}')
+print(f'The Difference of {num1} and {num2} is: {num_diff}')
+print(f'The Product of {num1} and {num2} is: {num_prod}')
+print(f'The Quotient of {num1} and {num2} is: {num_quo}')
